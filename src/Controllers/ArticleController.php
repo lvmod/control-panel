595f7549ae42176b92d6a88aacf50cfb7b76abd8
Юрий Шульгин <yu.shulgin@gmail.com>
2019-09-27 13:16:10 +0300

    Удаление неиспользуемых материалов

--- src/Controllers/ArticleController.php
+++ src/Controllers/ArticleController.php
@@ -7,9 +7,13 @@ use Lvmod\ControlPanel\Repositories\ArticleRepository;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Symfony\Component\DomCrawler\Crawler;
+use Illuminate\Support\Facades\Storage;
 
 class ArticleController extends Controller
 {
+
+    protected $disk = "media";
+    protected $materialsPath = "materials";
 
     /**
      * Экземпляр ArticleRepository.
@@ -53,7 +57,7 @@ class ArticleController extends Controller
             'type' => 'article',
             'id' => $article->id,
             'article' => $article
-            ]);
+        ]);
     }
 
     public function store(Request $request)
@@ -69,10 +73,10 @@ class ArticleController extends Controller
         $article->visible = !!$request->visible;
         $article->body = ""; //$request->body;
         $article->author_id = $request->user()->id;
-        $article->multimedia_id = $request->multimedia?$request->multimedia:null;
+        $article->multimedia_id = $request->multimedia ? $request->multimedia : null;
         $article->save();
 
-        return redirect('/control/article/edit/'.$article->id);
+        return redirect('/control/article/edit/' . $article->id);
     }
 
     public function update(Request $request, Article $article)
@@ -83,20 +87,38 @@ class ArticleController extends Controller
             'body' => 'required',
         ]);
 
-        // $crawler = new Crawler($request->body);
-
-        // $src = ($crawler->filter('img[src]')->each(function ($node) {
-        //      return $node->attr('src');
-        // }));
-        // return $src;
-        // return $src;
+        //Удаление не используемого материала
+        try {
+            $crawler = new Crawler($request->body);
+            $src = ($crawler->filter('img[src]')->each(function ($node) {
+                $path = $node->attr('src');
+                if ($path) {
+                    return basename($path);
+                }
+                return;
+            }));
+            $indoc = [];
+            foreach ($src as $value) {
+                if ($value) {
+                    $indoc[basename($value)] =  $value;
+                }
+            }
+            $files = Storage::disk($this->disk)->files($this->materialsPath . '/' . 'article' . '/' . $article->id);
+            foreach ($files as $file) {
+                if (!array_key_exists(basename($file), $indoc)) {
+                    Storage::disk($this->disk)->delete($file);
+                }
+            }
+        } catch (\Exception $ex) {
+            //throw $th;
+        }
 
         $article->title = $request->title;
         $article->posted = \Carbon\Carbon::parse($request->posted)->toDateString();
         $article->visible = !!$request->visible;
         $article->body = $request->body;
         $article->author_id = $request->user()->id;
-        $article->multimedia_id = $request->multimedia?$request->multimedia:null;
+        $article->multimedia_id = $request->multimedia ? $request->multimedia : null;
         $article->save();
 
         return redirect('/control/article');
