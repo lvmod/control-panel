c1320b99aea9dd2db5365a6c79b1cf43ce660621
Yuri Shulgin <yu.shulgin@gmail.com>
2019-10-15 11:04:49 +0300

    remove trigger

--- src/migrations/2019_08_20_091211_create_gallery_photo_table.php
+++ src/migrations/2019_08_20_091211_create_gallery_photo_table.php
@@ -28,18 +28,18 @@ class CreateGalleryPhotoTable extends Migration
             // $table->softDeletes(); //Мягкое удаление https://laravel.ru/docs/v5/eloquent#%D0%BC%D1%8F%D0%B3%D0%BA%D0%BE%D0%B5
         });
 
-        DB::unprepared("
-            CREATE TRIGGER tr_".$this->tableName."_default_priority BEFORE INSERT ON `".$this->tableName."` FOR EACH ROW
-            BEGIN
-                declare v_id bigint default 0;
+        // DB::unprepared("
+        //     CREATE TRIGGER tr_".$this->tableName."_default_priority BEFORE INSERT ON `".$this->tableName."` FOR EACH ROW
+        //     BEGIN
+        //         declare v_id bigint default 0;
             
-                select auto_increment into v_id
-                from information_schema.tables
-                where table_name = '".$this->tableName."'
-                and table_schema = database();
-                SET NEW.priority = v_id;
-            END
-        ");
+        //         select auto_increment into v_id
+        //         from information_schema.tables
+        //         where table_name = '".$this->tableName."'
+        //         and table_schema = database();
+        //         SET NEW.priority = v_id;
+        //     END
+        // ");
     }
 
     /**
@@ -49,7 +49,7 @@ class CreateGalleryPhotoTable extends Migration
      */
     public function down()
     {
-        DB::unprepared('DROP TRIGGER `tr_'.$this->tableName.'_default_priority`');
+        // DB::unprepared('DROP TRIGGER `tr_'.$this->tableName.'_default_priority`');
         Schema::dropIfExists($this->tableName);
     }
 }
--- src/migrations/2019_08_20_091212_create_gallery_photo_multimedia_table.php
+++ src/migrations/2019_08_20_091212_create_gallery_photo_multimedia_table.php
@@ -28,18 +28,18 @@ class CreateGalleryPhotoMultimediaTable extends Migration
             $table->timestamps();
         });
 
-        DB::unprepared("
-            CREATE TRIGGER tr_".$this->tableName."_default_priority BEFORE INSERT ON `".$this->tableName."` FOR EACH ROW
-            BEGIN
-                declare v_id bigint default 0;
+        // DB::unprepared("
+        //     CREATE TRIGGER tr_".$this->tableName."_default_priority BEFORE INSERT ON `".$this->tableName."` FOR EACH ROW
+        //     BEGIN
+        //         declare v_id bigint default 0;
             
-                select auto_increment into v_id
-                from information_schema.tables
-                where table_name = '".$this->tableName."'
-                and table_schema = database();
-                SET NEW.priority = v_id;
-            END
-        ");
+        //         select auto_increment into v_id
+        //         from information_schema.tables
+        //         where table_name = '".$this->tableName."'
+        //         and table_schema = database();
+        //         SET NEW.priority = v_id;
+        //     END
+        // ");
     }
 
     /**
@@ -49,7 +49,7 @@ class CreateGalleryPhotoMultimediaTable extends Migration
      */
     public function down()
     {
-        DB::unprepared('DROP TRIGGER `tr_'.$this->tableName.'_default_priority`');
+        // DB::unprepared('DROP TRIGGER `tr_'.$this->tableName.'_default_priority`');
         Schema::dropIfExists($this->tableName);
     }
 }
